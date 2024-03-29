b2c5353f366e7d220a4faeeae73ba72aaa33fee8
Vishalsuthar88 <vishaljangid.vs@gmail.com>
2024-03-30 00:31:24 +0530

    update Hero.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         <div className="container py-3 sm:py-0">
           <div className="flex justify-between items-center">
             <div>
-              <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2 ">
                { /*<img src={Logo} alt="Logo" className="w-20" />*/}
                 GreenAahar
               </a>
